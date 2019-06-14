6d943c3ccd5cf4c8fa85ae5ff54db9e22c187976
rafaeljordaojardim <jordao.jardim@hotmail.com.br>
2019-06-13 21:58:59 -0300

    Estrutura de controle - finalizado

---
+++ controle/for1.js
@@ -0,0 +1,14 @@
+let contador = 1
+while(contador <= 10){
+    console.log(`Contador = ${contador}`);
+    contador++
+}//while
+//equals 
+for  (let i = 1; i <=10; i++) {
+     console.log(`i = ${i}`);
+}//for
+
+const notas = [6.7, 7.5, 9.8, 8.1, 7.7]
+for (let i = 0; i < notas.length; i++) {
+    console.log(`nota = ${notas[i]}`);
+}
---
+++ controle/if2.js
@@ -0,0 +1,21 @@
+//feature
+//always use keys to separete the blocks
+function teste(num) {
+    if (num > 7) 
+        console.log(num);
+    console.log('Final');    
+    
+}
+
+teste(6)
+teste(8)
+
+//take care
+function teste2(num) {
+    if (num > 7); {// dont use ; in the end of sentences
+        console.log(num);
+        
+    }
+}
+teste2(6)
+teste2(8)
