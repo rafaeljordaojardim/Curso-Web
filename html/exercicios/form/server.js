a779c7498f18763f0a5b9e83fe061222420a04ee
rafaeljordaojardim <jordao.jardim@hotmail.com.br>
2019-06-27 07:50:45 -0300

    HTML acabado

---
+++ html/exercicios/form/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended:true }))
+
+app.post('/usuarios', (req, resp, next) =>{
+    console.log(req.body);
+    resp.send('<h1>Parabens, usuario incluido</h1>')
+})
+
+app.post('/usuarios/:id', (req, resp, next) =>{
+    console.log(req.params.id);
+    console.log(req.body);
+    resp.send('<h1>Parabens. usuario alterado</h1>')
+})
+
+app.listen(3003)
