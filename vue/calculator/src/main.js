ec21ab6ee8d8f64e933083bee38d6556a9aa4455
rafaeljordaojardim <jordao.jardim@hotmail.com.br>
2019-07-25 14:59:40 -0300

    vue finished

---
+++ vue/calculator/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App'
+
+
+new Vue({
+    //el:'#app',
+    render: h => h(App)
+    // render(createElement){
+    //     return createElement(App)
+    // }
+}).$mount('#app')
